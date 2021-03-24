66aae639bfa0eae43d84e8a325126a400b14d7fe
YezhikSunny <Sasha2010.008@mail.ru>
2021-03-25 00:07:22 +0300

    5 и 6 задания

--- scripts/script.js
+++ scripts/script.js
@@ -46,6 +46,10 @@ a = 2;
 let x = 1 + (a *= 2);
 /*3 задание
 */
+/**
+ * функция для генироравания случайного числа
+ * @returns - возвращает случайное число в диапозоне от -50 до 50
+ */
 function randomstart() {
    return parseInt(Math.random()*100 - 50);
 }
@@ -63,6 +67,38 @@ if (a>=0 && b>=0) {
 alert(s);
 /*4 задание
 */
+let p =  sum(a,b);
+alert('Сумма: ' + p);
+p =  subtraction(a,b);
+alert('Вычитание: ' + p);
+p =  multiplication(a,b);
+alert('Произведение: ' + p);
+p =  division(a,b);
+alert('Деление: ' + p);
+/*5 задание
+*/
+
+a = +prompt("Какое первое число?");
+b = +prompt("Какое второе число?");
+operate = prompt("Какую операцию хотите выполнить: сложение, вычитание, умножение или деление?");
+let result = mathOperation(a, b, operate);
+alert(result);
+/*6 задание
+*/
+let money = prompt("Сколько денег Вы хотите положить в банк?");
+let k = money.charAt(money.length - 1);
+k = parseInt(k);
+moneyinbank(k, money);
+//функции
+//функция 3 задания
+/**
+ * функция для генироравания случайного числа
+ * @returns - возвращает случайное число в диапозоне от -50 до 50
+ */
+ function randomstart() {
+    return parseInt(Math.random()*100 - 50);
+ }
+//функции 4 задания
 function sum(a,b) {
     return a + b;
 }
@@ -75,16 +111,59 @@ function multiplication(a,b) {
 function division(a,b) {
     return a / b;
 }
-let p =  sum(a,b);
-alert('Сумма: ' + p);
-p =  subtraction(a,b);
-alert('Вычитание: ' + p);
-p =  multiplication(a,b);
-alert('Произведение: ' + p);
-p =  division(a,b);
-alert('Деление: ' + p);
-/*5 задание
-*/
-/*6 задание
-*/
+//функции 5 задания
+function mathOperation(arg1, arg2, operation) {
+    switch(operation) {
+        case 'сложение':
+            return  sum(a,b);
+        
+        case 'вычитание':
+            return  subtraction(a,b);
+        
+        case 'умножение':
+            return  multiplication(a,b);
 
+        case 'деление':
+            return  division(a,b);
+        default:
+            alert("Введите одно из приведённых значений");
+            operate = prompt("Какую операцию хотите выполнить: сложение, вычитание, умножение или деление?");
+    }
+}
+//функции 6 задания
+function moneyinbank(lastmoney,moneyk){
+    switch (lastmoney) {
+        case 0:
+            alert("Ваша сумма в " + moneyk +" рублей успешно зачислена.");
+            return;
+        case 1:
+            alert("Ваша сумма в " + moneyk +" рубль успешно зачислена.");
+            return;
+        case 2:
+            alert("Ваша сумма в " + moneyk +" рубля успешно зачислена.");
+            return;
+        case 3:
+            alert("Ваша сумма в " + moneyk +" рубля успешно зачислена.");
+            return;
+        case 4:
+            alert("Ваша сумма в " + moneyk +" рубля успешно зачислена.");
+            return;
+        case 5:
+            alert("Ваша сумма в " + moneyk +" рублей успешно зачислена.");
+            return;
+        case 6:
+            alert("Ваша сумма в " + moneyk +" рублей успешно зачислена.");
+            return;
+        case 7:
+            alert("Ваша сумма в " + moneyk +" рублей успешно зачислена.");
+            return;
+        case 8:
+            alert("Ваша сумма в " + moneyk +" рублей успешно зачислена.");
+            return;
+        case 9:
+            alert("Ваша сумма в " + moneyk +" рублей успешно зачислена.");
+            return;
+        default:
+            alert('Нужно ввести сумму.'); 
+    }   
+}
