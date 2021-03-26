41a2a19d5384919cf61d89ba24f0fc639fc55323
YezhikSunny <Sasha2010.008@mail.ru>
2021-03-26 17:29:18 +0300

    1 задание

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
-"use ctrict";
+"use strict";
+// 1 задание
+console.log(0 + ' -  это ноль');
+for(let i = 1;i<=10;i++){
+    if(i%2 == 0) {
+        console.log(i + ' - чётное число');
+    } else {
+        console.log(i + ' - нечётное число');
+    }
+}
